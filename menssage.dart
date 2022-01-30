efba9817b938c176032cf698c221f0fee208b957
Francis Santos <francis.source@gmail.com>
2022-01-30 17:37:06 -0300

    Subindo nova função.

--- menssage.dart
+++ menssage.dart
@@ -1,3 +1,19 @@
 void main (){
-  print("Hello");
+
+  menssage('Francis',cliente : 'Juliano');
 }
+
+
+void  menssage(String consultor,{String? cliente}){
+  if (cliente == null) {
+    print("Bom dia consultor, ${consultor}!\nPor enquanto você não tem cliente para atender.");
+  } else {
+    print("Bem vindo, ${cliente}!\nHoje o seu consultor será ${consultor}");
+  }
+   
+}
+
+
+
+
+
