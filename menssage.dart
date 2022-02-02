c3b638fbb017d387745bbd7b3bd7fe1167363b0b
Francis Santos <francis.source@gmail.com>
2022-02-02 17:05:23 -0300

    subindo modificações

--- menssage.dart
+++ menssage.dart
@@ -1,19 +1,25 @@
-void main (){
-
-  menssage('Francis',cliente : 'Juliano');
+void main() {
+  menssage(
+    'Francis',
+    cliente: 'Juliano',
+    contador: funcaoContador,
+  );
 }
 
+String funcaoContador(marca) => "A marca $marca agracede!";
 
-void  menssage(String consultor,{String? cliente}){
+// função nomeada não posicional com váriavel nula e váriavel função requerida;
+
+void menssage(
+  String consultor, {
+  String? cliente,
+  required Function(String) contador,
+}) {
   if (cliente == null) {
-    print("Bom dia consultor, ${consultor}!\nPor enquanto você não tem cliente para atender.");
+    print(
+        "Bom dia consultor, ${consultor}!\nPor enquanto você não tem cliente para atender.");
   } else {
-    print("Bem vindo, ${cliente}!\nHoje o seu consultor será ${consultor}");
+    print("Bem vindo, ${cliente}!\nHoje o seu consultor será ${consultor}.");
   }
-   
+  contador('Ford');
 }
-
-
-
-
-
